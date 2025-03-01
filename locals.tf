575f31a462a1816ec2ecf1a7fc565dca92eb0758
ywatanabe <yu.w.tennis@gmail.com>
2025-03-01 16:05:14 +0900

    fix: Missing field in payload.

--- locals.tf
+++ locals.tf
@@ -6,6 +6,7 @@ locals {
   bq_raw_data_tbl_name         = "flights_raw"
 
   ingest_flights_monthly_req_body = {
+    project_id       = data.google_project.project.project_id
     bucket           = google_storage_bucket.staging.name
     dest_bq_tbl_fqdn = "${local.bq_data_science_dataset_name}.${local.bq_raw_data_tbl_name}"
   }
